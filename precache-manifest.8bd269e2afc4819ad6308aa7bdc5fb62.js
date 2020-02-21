4546c357be0945fea4cbd9ca10e71d4d33ab9851
Kacper <kacpermak24@gmail.com>
2020-02-21 17:05:35 +0000

    Updates

--- precache-manifest.8bd269e2afc4819ad6308aa7bdc5fb62.js
+++ precache-manifest.8bd269e2afc4819ad6308aa7bdc5fb62.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b147a090fb6d45e8a9255e78dfd0850",
+    "revision": "6866f78c56f48a2af3af9e7dfb2047ce",
     "url": "/multiSearchApp/index.html"
   },
   {
-    "revision": "eb3712de7f8c56e596b5",
+    "revision": "42d35d7d1e4844a0efd7",
     "url": "/multiSearchApp/static/css/main.e4c19e2e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/multiSearchApp/static/js/2.b38a128c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eb3712de7f8c56e596b5",
-    "url": "/multiSearchApp/static/js/main.35ef1cdd.chunk.js"
+    "revision": "42d35d7d1e4844a0efd7",
+    "url": "/multiSearchApp/static/js/main.3129415d.chunk.js"
   },
   {
     "revision": "81ba657002fe9ac5522d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multiSearchApp/precache-manifest.940d4d877aeb5091b73964c25a9ac719.js"
+  "/multiSearchApp/precache-manifest.8bd269e2afc4819ad6308aa7bdc5fb62.js"
 );
 
 self.addEventListener('message', (event) => {
